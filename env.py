5aebcf974728e18cfe3c6097de002333243516b1
Paolo Jovon <paolo.jovon@gmail.com>
2021-05-25 15:13:20 +0100

    Add a env-backend Var type

---
+++ env.py
@@ -0,0 +1,50 @@
+import os
+from typing import Any, Callable, Generic, TypeVar, Optional, Dict
+
+import dotenv
+dotenv.load_dotenv()
+
+all_vars: Dict[str, 'Var[T]'] = {}
+_var_t = TypeVar('T')
+
+
+class Var(Generic[_var_t]):
+    def __init__(self,
+                 key: str,
+                 default: Optional[_var_t] = None,
+                 type: Optional[Callable[[str], _var_t]] = None,
+                 optional: bool = True,
+                 help: str = None):
+        self.key: str = key
+        self.help: str = help
+        self.default: Optional[_var_t] = default
+        self.value: Optional[_var_t] = default
+        self.type: Optional[Callable[[str], _var_t]] = type or globals()['type'](default)
+        self.optional: bool = optional
+        self.sync()
+        all_vars[self.key] = self
+
+    def sync(self):
+        self.value: Optional[_var_t] = os.getenv(self.key)
+        if self.value is not None:
+            self.value = self.type(self.value) if self.type is not None else self.value
+        elif self.optional:
+            self.value = self.default
+        else:
+            raise KeyError(f"${{{self.key}}} not set\n{self}")
+
+    def __str__(self) -> str:
+        doc = f"{self.key}"
+        extras = ["optional"] * self.optional
+        extras += [f"default = {self.default}"] * bool(self.default)
+        extras += [f"type = {self.type.__name__}"] * bool(self.type)
+        doc += f" [{','.join(extras)}]" if extras else ""
+        if self.help:
+            doc += f"\n\t{self.help}"
+        return doc
+
+    def __repr__(self) -> str:
+        return f'Var({self.key}={repr(self.value)})'
+
+    def __get__(self, instance: 'Var', owner: Any) -> Optional[_var_t]:
+        return self.value
