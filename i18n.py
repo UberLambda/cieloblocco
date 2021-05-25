c741b228ab9b4a7a848b34d2ded4bdc3f98de367
Paolo Jovon <paolo.jovon@gmail.com>
2021-05-25 16:09:20 +0100

    Improved i18n

--- i18n.py
+++ i18n.py
@@ -2,9 +2,14 @@ import gettext
 from pathlib import Path
 
 # TODO: Search the system-wide, /usr/share/locale if installed?
-locale_dir = Path(__file__).parent() / 'locale'
+locale_dir = Path(__file__).parent / 'locale'
 gettext.bindtextdomain('cieloblocco', locale_dir)
 gettext.textdomain('cieloblocco')
 
-tr = gettext.gettext
-trn = gettext.ngettext
+
+def tr(key: str, *args, **kwargs) -> str:
+    return gettext.gettext(key).format(*args, **kwargs)
+
+
+def trn(singular: str, plural: str, *args, **kwargs) -> str:
+    return gettext.ngettext(singular, plural).format(*args, **kwargs)
